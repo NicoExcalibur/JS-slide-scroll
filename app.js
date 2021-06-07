4a4e2ea51d5f619033b5a5e587fff61b589b9dd1
NicoExcalibur <nico.zanz@gmail.com>
2021-06-07 11:00:40 +0200

    select images and create a function at scroll

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+// select all images
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  console.log(e);
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
